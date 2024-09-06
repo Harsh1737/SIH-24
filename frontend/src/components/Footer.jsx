f165ab762c0a868f1571ccc312bff589df7db09e
Harsh <harshbit10@gmail.com>
2024-09-06 19:44:43 +0530

    imporved css structure

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,48 +1,73 @@
-import playStore from '../assets/play-store.png'
-import appStore from '../assets/app-store.png'
-import '../index.css'
+import playStore from "../assets/play-store.png";
+import appStore from "../assets/app-store.png";
+import "../index.css";
 
 function Footer() {
-    return (
-        <div className='mt-10'>
-            <div className="footer">
-                <div className="container">
-                    <div className="row">
-                        <div className="footer-col-1">
-                            <h3>Download Our App</h3>
-                            <p>Download App for Android and iOS.</p>
-                            <div className="app-logo">
-                                <img src={playStore} alt="Play Store" className="w-32 h-auto mt-5" />
-                                <img src={appStore} alt="App Store" className="w-32 h-auto mt-2" />
-                            </div>
-                        </div>
-                        <div className="footer-col-2">
-                            <img src="" alt="Logo" />
-                        </div>
-                        <div className="footer-col-3">
-                            <h3>Useful Links</h3>
-                            <ul>
-                                <li>Coupons</li>
-                                <li>Blog Post</li>
-                                <li>Return Policy</li>
-                                <li>Join Affiliate</li>
-                            </ul>
-                        </div>
-                        <div className="footer-col-4 gap-4">
-                            <h3>Follow Us</h3>
-                            <ul>
-                                <li>Facebook</li>
-                                <li>Twitter</li>
-                                <li>Instagram</li>
-                                <li>Join Affiliate</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="mt-10">
+      <div className="bg-[#37022a] text-white py-10 font-sans">
+        <div className="max-w-[1200px] mx-auto px-[25px]">
+          <div className="flex flex-wrap justify-between">
+            <div className="flex-basis-[25%] min-w-[200px] mb-[20px]">
+              <h3 className="text-[18px] mb-[15px]">Download Our App</h3>
+              <p className="text-[14px] leading-[24px]">
+                Download App for Android and iOS.
+              </p>
+              <div>
+                <img
+                  src={playStore}
+                  alt="Play Store"
+                  className="w-32 h-auto mt-2 mr-[10px]"
+                />
+                <img
+                  src={appStore}
+                  alt="App Store"
+                  className="w-32 h-auto mt-2 mr-[10px]"
+                />
+              </div>
             </div>
+            <div className="flex-basis-[25%] min-w-[200px] mb-[20px]">
+              <img src="" alt="Logo" />
+            </div>
+            <div className="footer-col-3 flex-basis-[25%] min-w-[200px] mb-[20px]">
+              <h3 className="text-[18px] mb-[15px]">Useful Links</h3>
+              <ul>
+                <li className="text-[14px] leading-[24px] cursor-pointer hover:text-[#ff523b]">
+                  Coupons
+                </li>
+                <li className="text-[14px] leading-[24px] cursor-pointer hover:text-[#ff523b]">
+                  Blog Post
+                </li>
+                <li className="text-[14px] leading-[24px] cursor-pointer hover:text-[#ff523b]">
+                  Return Policy
+                </li>
+                <li className="text-[14px] leading-[24px] cursor-pointer hover:text-[#ff523b]">
+                  Join Affiliate
+                </li>
+              </ul>
+            </div>
+            <div className="footer-col-4 flex-basis-[25%] min-w-[200px] mb-[20px] gap-4">
+              <h3 className="text-[18px] mb-[15px]">Follow Us</h3>
+              <ul>
+                <li className="mb-[10px] text-[14px] leading-[24px] cursor-pointer hover:text-[#ff523b]">
+                  Facebook
+                </li>
+                <li className="mb-[10px] text-[14px] leading-[24px] cursor-pointer hover:text-[#ff523b]">
+                  Twitter
+                </li>
+                <li className="mb-[10px] text-[14px] leading-[24px] cursor-pointer hover:text-[#ff523b]">
+                  Instagram
+                </li>
+                <li className="mb-[10px] text-[14px] leading-[24px] cursor-pointer hover:text-[#ff523b]">
+                  Join Affiliate
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Footer
-
+export default Footer;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,61 +1,79 @@
-import "../index.css";
 import { Link } from "react-router-dom";
-import '../index.css';
+import "../index.css";
 
 function Navbar() {
   return (
-    <div id="hero">
-      <header id="main-header">
+    <>
+      <div className="flex justify-between align-middle mx-0 my-auto w-[90%]">
         <a href="/">
           <img src="images/logo-l5.svg" alt="Logo" />
         </a>
 
         <nav className="flex flex-wrap justify-around">
-          <ul>
+          <ul className="list-none p-0 m-0 flex gap-12">
             <li className="px-0">
-              <a href="#">Jobs</a>
+              <a href="#" className="no-underline inline-block py-6">
+                Jobs
+              </a>
             </li>
+
             <div className="w-[1px] h-16 bg-gray-100 "></div>
 
-            <li className="has-submenu">
-              <a href="#">Services</a>
-              <ul>
+            <li className="has-submenu relative flex gap-2 items-center justify-start pt-0 pb-6">
+              <a href="#" className="no-underline inline-block py-6 ">
+                Services
+              </a>
+              <ul className="absolute top-full left-0 bg-custom-gradient rounded-md p-2 hidden hover:text-white">
                 <li>
-                  <a href="#">Logo Design</a>
+                  <a href="#" className="no-underline py-6 flex justify-between items-center p-4 min-w-[15rem] rounded-md text-black ">
+                    Logo Design
+                  </a>
                 </li>
                 <li>
-                  <a href="#">Web Development</a>
+                  <a href="#" className="no-underline py-6 flex justify-between items-center p-4 min-w-[15rem] rounded-md text-black">
+                    Web Development
+                  </a>
                 </li>
                 <li>
-                  <a href="#">Photography</a>
+                  <a href="#" className="no-underline py-6 flex justify-between items-center p-4 min-w-[15rem] rounded-md text-black">
+                    Photography
+                  </a>
                 </li>
                 <li>
-                  <a href="#">Content Creator</a>
+                  <a href="#" className="no-underline py-6 flex justify-between items-center p-4 min-w-[15rem] rounded-md text-black">
+                    Content Creator
+                  </a>
                 </li>
                 <li>
-                  <a href="#">Video and Animation</a>
+                  <a href="#" className="no-underline py-6 flex justify-between items-center p-4 min-w-[15rem] rounded-md text-black">
+                    Video and Animation
+                  </a>
                 </li>
               </ul>
             </li>
             <div className="w-[1px] h-16 bg-gray-100  "></div>
             <li>
-              <a href="#">What's New</a>
+              <a href="#" className="no-underline inline-block py-6">
+                What's New
+              </a>
             </li>
             <div className="w-[1px] h-16 bg-gray-100 "></div>
             <li>
-              <a href="#">About</a>
+              <a href="#" className="no-underline inline-block py-6">
+                About
+              </a>
             </li>
 
-            <Link to="/login" >
-              <button className="h-10 w-24 px-4 py-2 bg-blue-400 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition-colors duration-300">
+            <Link to="/login">
+              <button className="h-10 w-24 mt-5 px-6 py-0 bg-blue-400 text-white font-semibold rounded-lg shadow-md hover:bg-blue-600 transition-colors duration-300">
                 Login
               </button>
             </Link>
           </ul>
         </nav>
-      </header>
+      </div>
       <div className="w-full h-[1px] bg-gray-100 "></div>
-    </div>
+    </>
   );
 }
 
