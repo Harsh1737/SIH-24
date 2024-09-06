292a19c40fce7171e6070eca75a15effd6028898
Harsh <harshbit10@gmail.com>
2024-09-06 19:47:38 +0530

    config tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(to bottom, #a0c4ff, #5fa8d3, #48bfe3, #caf0f8)",
+      },
+    },
   },
   plugins: [],
-}
+};
