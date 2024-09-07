a7c62e26e7bda1b667b15f9a7d55f99999615c50
Harsh <harshbit10@gmail.com>
2024-09-07 18:55:22 +0530

    imported updates from assets

---
+++ frontend/src/assets/index.js
@@ -0,0 +1,73 @@
+import appstore from "./app-store.png";
+import bg from "./bg.svg";
+import iii from "./iii.jpg";
+import im from "./im.jpg";
+import image1 from "./image1.jpg";
+import image2 from "./image2.png";
+import image3 from "./image3.jpg";
+import image4 from "./image4.jpg";
+import image5 from "./image5.jpg";
+import image6 from "./image6.png";
+import image7 from "./image7.png";
+import image8 from "./image8.png";
+import image9 from "./image9.png";
+import image10 from "./image10.png";
+import image11 from "./image11.png";
+import image12 from "./image12.png";
+import image13 from "./image13.png";
+import image14 from "./image14.png";
+import image15 from "./image15.png";
+import image16 from "./image16.png";
+import image17 from "./image17.png";
+import image18 from "./image18.png";
+import image19 from "./image19.png";
+import image20 from "./image20.png";
+import image21 from "./image21.png";
+import img from "./img.jpg";
+import jobs from "./jobs.png";
+import nav from "./nav.png";
+import navbar from "./navbar.png";
+import pencil from "./pencil.svg";
+import playstore from "./play-store.png";
+import rating from "./rating.png";
+import react from "./react.svg";
+import svg from "./svg.jpg";
+
+const images = {
+  appstore,
+  bg,
+  iii,
+  im,
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+  image10,
+  image11,
+  image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  img,
+  jobs,
+  nav,
+  navbar,
+  pencil,
+  playstore,
+  rating,
+  react,
+  svg,
+};
+
+export default images;
